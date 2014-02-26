fd3df04470e00da80aeeb35dd93a62bc7ecdc25e
jakelamotta <kristian@stimuli-hp.(none)>
2014-02-26 17:03:56 +0100

    a

---
+++ code/matlab/model/Configuration.m
@@ -0,0 +1,46 @@
+%Configuration class, singleton object that encapsulates configuration
+%parameters. 
+classdef Configuration < handle
+    %CONFIGURATION Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (GetAccess = 'public',SetAccess = 'private')
+        trigger;
+        port;
+        timer;
+        runnable;
+        time;
+        savepath;
+    end
+    
+    methods (Access = 'public')
+        
+        %Constructor for the config object
+        function this = Configuration()
+            this.port = 4444;
+            this.trigger = 'no';
+            this.runnable = true;
+            time = 3000;
+            this.savepath = '/home/kristian/master_thesis/data';
+        end   
+        
+        %%%%%%%%%%SETTERS%%%%%%%%%%%%%%%%
+        function trigger = setNetworkTrigger(this,t)
+            this.trigger = t;
+        end
+        
+        function runnable = setRunnable(this,bool)
+            this.runnable = bool;
+        end
+        
+        function savepath = setPath(this,path)
+            this.savepath = path;
+        end
+        
+        function time = setTime(this,t)
+            this.time = t;
+        end
+    end
+    
+end
+
---
+++ code/matlab/model/calc2DPath.m
@@ -0,0 +1,7 @@
+function [path] = calc2DPath(data)
+%CALC2DPATH Function that calculates a 2D path given delta coordinates for
+%the fly
+%   Detailed explanatwion goes here
+
+end
+
